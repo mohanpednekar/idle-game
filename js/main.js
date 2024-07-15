2254a9f94831bebec6dc8f2fabf5454c68a5e4ba
Mohan Namdeo Pednekar <mohanpednekar@users.noreply.github.com>
2024-07-16 01:41:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 let number = 0;
 let increment = 1
-let intervalId = setInterval(incrementNumber, 5000); // 5 seconds interval
+let cycle = 5 // seconds
+let intervalId = setInterval(incrementNumber, cycle*1000);
 
 function incrementNumber() {
   number+=increment;
   document.getElementById('numberDisplay').innerText = number;
-  document.getElementById('progress').style.width = '0%'; // Reset progress bar
-  setTimeout(() => {
-    document.getElementById('progress').style.width = '100%';
-  }, 50); // Small delay to reset and start filling again
+  
+  let progressWidth = 0;
+  let incrementAmount = 1; // Adjust increment amount as needed for smoothness
+
+  let interval = setInterval(() => {
+    progressWidth += incrementAmount;
+    document.getElementById('progress').style.width = `${progressWidth}%`;
+
+    if (progressWidth >= 100) {
+      clearInterval(interval); // Stop the interval once progress reaches 100%
+    }
+  }, cycle * 10);
 }
 
 document.getElementById('upgradeButton').addEventListener('click', () => {
